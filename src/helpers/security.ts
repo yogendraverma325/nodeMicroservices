cacd9d6094f92aee73916a5121ae7933ec78a30b
aman.jha1 <aman.jha@teamcomputers.com>
2024-01-12 23:14:40 +0530

    fix: typo mistake

--- src/helpers/security.ts
+++ src/helpers/security.ts
@@ -5,6 +5,6 @@ import { findIpAddress } from './utils.js';
 export function restrictIpAddress(req: Request, ipAddress: string) {
   if (ipAddress === '*') return;
   const ip = findIpAddress(req);
-  if (!ip) throw new ForbiddenError('IP Address Not Recognised');
+  if (!ip) throw new ForbiddenError('IP Address Not Recognized');
   if (ipAddress !== ip) throw new ForbiddenError('Permission Denied');
 }
