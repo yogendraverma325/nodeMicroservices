7e2c3c7ebc6f9eed8b54ade43e45c86a4f8b63ec
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-01-15 22:20:41 +0530

    chore : updated mongoose config from database/index.ts file since we are using external loader and the file entity is separate

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -1,17 +1,64 @@
 import mongoose, { ConnectOptions } from 'mongoose';
+import Logger from '../core/Logger.js';
 
 const mongooseLoader = async (): Promise<void> => {
-  const options: ConnectOptions = {};
+  const options: ConnectOptions = {
+    autoIndex: true,
+    // Maintain up to x socket connections
+    connectTimeoutMS: 60000, // Give up initial connection after 10 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  };
+
+  Logger.debug(process.env.LOCAL_DB_URI);
+
+  function setRunValidators() {
+    this.setOptions({ runValidators: true });
+  }
+
   //  `mongoose.set('strictQuery', true);` to suppress this warning.
   mongoose.set({ strictQuery: true });
-  mongoose.connect(process.env.LOCAL_DB_URI as string, options);
-  const connection = mongoose.connection;
-  connection.once('open', (_) => {
-    console.log('Database connected:', process.env.LOCAL_DB_URI);
+  // Create the database connection
+  mongoose
+    .plugin((schema: any) => {
+      schema.pre('findOneAndUpdate', setRunValidators);
+      schema.pre('updateMany', setRunValidators);
+      schema.pre('updateOne', setRunValidators);
+      schema.pre('update', setRunValidators);
+    })
+    .connect(process.env.LOCAL_DB_URI as string, options)
+    .then(() => {
+      Logger.info('Mongoose connection done');
+    })
+    .catch((e) => {
+      Logger.info('Mongoose connection error');
+      Logger.error(e);
+    });
+
+  // CONNECTION EVENTS
+  // When successfully connected
+  mongoose.connection.on('connected', () => {
+    Logger.debug(
+      'Mongoose default connection open to ' + process.env.LOCAL_DB_URI,
+    );
   });
 
-  connection.on('error', (err) => {
-    console.error('connection error:', err);
+  // If the connection throws an error
+  mongoose.connection.on('error', (err) => {
+    Logger.error('Mongoose default connection error: ' + err);
+  });
+
+  // When the connection is disconnected
+  mongoose.connection.on('disconnected', () => {
+    Logger.info('Mongoose default connection disconnected');
+  });
+  // If the Node process ends, close the Mongoose connection
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      Logger.info(
+        'Mongoose default connection disconnected through app termination',
+      );
+      process.exit(0);
+    });
   });
 };
 
